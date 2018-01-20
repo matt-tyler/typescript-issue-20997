ea87be88bb8849c5a437acdea39fefb144ae1500
Matt Tyler <me@matthewtyler.io>
2018-01-20 13:00:23 +0800

    remove external dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,39 @@
-import * as AWS from 'aws-sdk'
-import { isUndefined } from 'util'
-
-export async function DoSomething(): Promise<Array<string>> {
-  const principalARNs = new Array<string>()
-
-  const serviceCatalog = new AWS.ServiceCatalog()
-  const portfolioDetails = await Util.All(serviceCatalog.listPortfolios, r => r.PortfolioDetails)
-
-  for (const portfolioDetail of portfolioDetails) {
-    if (isUndefined(portfolioDetail.Id)) {
-      throw new Error('Portfolio ID is undefined')
-    }
-
-    const principals = await Util.All(serviceCatalog.listPrincipalsForPortfolio, r => r.Principals, { PortfolioId: portfolioDetail.Id })
-    principalARNs.push(...principals.map(p => p.PrincipalARN))
-  }
-
-  return principalARNs
+declare class Service {
+  constructor()
+  sequencer(n: SequenceRequest, callback?: (n: SequenceResponse) => void): SequenceResponse
+  sequencer(callback?: (n: SequenceResponse) => void): SequenceResponse 
 }
 
-export namespace Util {
-  
-    export interface PageableResponse {
-      NextPageToken?: string
-    }
-    
-    export interface PageableRequest {
-      PageToken?: string
-    }
-  
-    export type Callback<U> = (err: AWS.AWSError, data: U) => void
-  
-    export type Lister<T extends PageableRequest, U extends PageableResponse> = (
-      params: T,
-      callback?: Callback<U>
-    ) => AWS.Request<U, AWS.AWSError>
-  
-    export async function All<T, U extends PageableResponse, V> (
-      lister: Lister<T, U>,
-      fn: (obj: U) => V[] | undefined,
-      init = {} as T
-    ): Promise<Array<V>> {
-      const list = new Array<V[]>()
-      let params = init as PageableRequest
-      do {
-        const response = (await lister(params as T).promise())
-        list.push((fn(response) || []))
-        params = { PageToken: response.NextPageToken }
-      } while (!isUndefined(params.PageToken))
-      return list.reduce((a, b) => a.concat(b), [])
-    }
-  
-    export function isDefined<T> (x: T | undefined): x is T {
-      return !isUndefined(x)
-    }
-  }
+declare interface SequenceRequest {
+  A: Number
+  PageToken?: string
+}
+
+declare interface SequenceResponse {
+  B: Number[]
+  NextPageToken?: string
+}
+
+interface PageableResponse {
+  NextPageToken?: string
+}
+
+interface PageableRequest {
+  PageToken?: string
+}
+
+type Lister<T extends PageableRequest, U extends PageableResponse> = (
+  params: T,
+  callback?: (data: U) => void
+) => U
+
+declare function All<T, U, V>(
+  lister: Lister<T, U>,
+  fn: (obj: U) => V[] | undefined,
+  init?: T
+): Promise<Array<V>> 
+
+export async function main() {
+  const myService = {} as Service
+  const result = await All(myService.sequencer, r => r.B)
+}
